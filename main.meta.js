d4438d02f66bb73dcdc3aeddfc94cd5063afde1c
carrot <carrot.moncher@gmail.com>
2022-06-23 19:36:03 +0800

    Create main.meta.js

---
+++ main.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         aLtErNaTiNg CaPs
+// @version      0.1.1
+// @updateURL    https://raw.githubusercontent.com/AnnikaV9/aLtErNaTiNg-CaPs/master/main.meta.js
+// @downloadURL  https://raw.githubusercontent.com/AnnikaV9/aLtErNaTiNg-CaPs/master/main.user.js
+// @description  A dumb userscript that lets you select text in input and textarea fields and apply alternating caps to it.
+// @author       AnnikaV9
+// @include      *
+// @run-at       context-menu
+// @grant        none
+// ==/UserScript==
